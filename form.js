7e42a9d767280116fcfec6e8de2f572429da7e01
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-06-11 15:46:18 +0530

    form with node

--- form.js
+++ form.js
@@ -1,20 +1,47 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req,resp)=>{
-   console.log("apple");
-   if(req.url=='/'){
- resp.writeHead(200,{'content-type':'text/html'})
-   resp.write(`<form action='/submit' method='post' ><input name='email' type="text" />
-    <input  name='password' type="password" />
+
+
+http.createServer((req, resp)=>{
+    fs.readFile('html/form.html','utf-8',(err,data)=>{
+        if(err){
+            resp.end('internal server error')
+            return false;
+        }
+        
+        if(req.url=="/"){
+resp.writeHead(200,{"content-type":"text/html"})
+    resp.write(data)
+        resp.end();
+    }else{
+        resp.write(`
+       <h1>data submit</h1>
+        `)
+        resp.end();
+    }
+    })
+   
     
-    <button>Click me</button>
-    </form>`)
-   }
-   else{
- resp.writeHead(200,{'content-type':'text/html'})
-   resp.write(`<h1>Form Submitted</h1>`)
-   }
-  
-   resp.end()
-}).listen(3200) 
+
+}).listen(3200);
+
+
+// http.createServer((req, resp)=>{
+//     if(req.url=="/"){
+// resp.writeHead(200,{"content-type":"text/html"})
+//     resp.write(`
+//         <form  action="/submit" method="post" >
+//         <input type="text" placeholder="enter name" name="name" />
+//         <input type="text" placeholder="enter email" name="email" />
+//         <button>Submit</button>
+//         </form>
+//         `)
+//     }else{
+//         resp.write(`
+//        <h1>data submi</h1>
+//         `)
+//     }
+    
+// resp.end();
+// }).listen(3200);
