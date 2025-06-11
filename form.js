d66fac448426d7d75ee5f0c2b3deb7f5654a44a0
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-06-11 18:39:45 +0530

    done

--- form.js
+++ form.js
@@ -1,47 +1,44 @@
 const http = require('http');
 const fs = require('fs');
 
+http.createServer((req, resp) => {
+
+        fs.readFile('html/form.html', 'utf-8', (error, data) => {
+
+            if (error) {
+                resp.writeHead(500, { "content-type": 'text/plain' })
+                resp.end('internal server error')
+
+                return;
+
+            }
+            resp.writeHead(200, { "content-type": 'text/html' })
+
+            if (req.url == '/') {
+                resp.write(data);
+            } else if (req.url == '/submit') {
+                resp.write('<h1>Data submitted</h1>')
+            }
+            resp.end()
+        })
 
 
-http.createServer((req, resp)=>{
-    fs.readFile('html/form.html','utf-8',(err,data)=>{
-        if(err){
-            resp.end('internal server error')
-            return false;
-        }
-        
-        if(req.url=="/"){
-resp.writeHead(200,{"content-type":"text/html"})
-    resp.write(data)
-        resp.end();
-    }else{
-        resp.write(`
-       <h1>data submit</h1>
-        `)
-        resp.end();
-    }
-    })
-   
-    
+    }).listen(3200)
+    // http.createServer((req, resp) => {
+    //     resp.writeHead(200, { "content-type": 'text/html' })
+    //     console.log(req.url);
 
-}).listen(3200);
-
-
-// http.createServer((req, resp)=>{
-//     if(req.url=="/"){
-// resp.writeHead(200,{"content-type":"text/html"})
-//     resp.write(`
-//         <form  action="/submit" method="post" >
-//         <input type="text" placeholder="enter name" name="name" />
-//         <input type="text" placeholder="enter email" name="email" />
+//     if (req.url == '/') {
+//         resp.write(`
+//         <form action="/submit" method="post">
+//         <input type="text" placeholder="enter name" name='name' />
+//         <input type="text" placeholder="enter email" name='email' />
 //         <button>Submit</button>
 //         </form>
-//         `)
-//     }else{
-//         resp.write(`
-//        <h1>data submi</h1>
-//         `)
+//         `);
+//     } else if (req.url == '/submit') {
+//         resp.write('<h1>Data submitted</h1>')
 //     }
-    
-// resp.end();
-// }).listen(3200);
+
+//     resp.end()
+// }).listen(3200)
