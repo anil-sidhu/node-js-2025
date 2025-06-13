0f09ed3d5349875b373630e5d843e395a6549fa2
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2025-06-13 12:49:41 +0530

    worked on async and sync

--- form.js
+++ form.js
@@ -27,7 +27,21 @@ http.createServer((req, resp) => {
                 req.on('end', () => {
                     let rawData = Buffer.concat(dataBody).toString();
                     let readableData = queryString.parse(rawData);
-                    console.log(readableData);
+                    let dataString = "My name is " + readableData.name + " and my email id is" + readableData.email;
+                    console.log(dataString);
+                    // fs.writeFileSync("text/" + readableData.name + ".txt", dataString);
+                    // console.log("file created");
+
+                    fs.writeFile("text/" + readableData.name + ".txt", dataString, 'utf-8', (err) => {
+                        if (err) {
+                            resp.end("internal server error")
+                            return false;
+                        } else {
+                            console.log("file created");
+                        }
+                    })
+
+
 
                 })
                 resp.write('<h1>Data submitted</h1>')
